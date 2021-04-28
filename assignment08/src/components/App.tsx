4979c666c99a433ca49aeedd45f6cc92ef5a8754
Christoph Fritz <fritz.chris@hotmail.com>
2021-04-28 16:46:31 +0200

    setup project

---
+++ assignment08/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import { autorun } from 'mobx';
+import { observer } from 'mobx-react';
+
+export const App = (): JSX.Element => {
+    return (
+        <p>Hello</p>
+    );
+};
