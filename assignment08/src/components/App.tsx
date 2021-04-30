442b230ef9f9fef54eaa03c3a04ec0506dc12825
Christoph Fritz <fritz.chris@hotmail.com>
2021-04-30 20:24:43 +0200

    implemented Store

--- assignment08/src/components/App.tsx
+++ assignment08/src/components/App.tsx
@@ -1,27 +1,62 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { autorun } from "mobx";
-import { observer } from "mobx-react";
-import { GlobalStyles } from '../styles/GlobalStyles'
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { observer, useLocalObservable } from 'mobx-react';
+import { Store } from '../model/Store';
+import { GlobalStyles } from '../styles/GlobalStyles';
 import { NavigationHeader } from './NavigationHeader';
 
-const API_KEY = "XdaoZq9JMeywVQvulWH47Yv2ZPAY6AC0";
+// Implement a viewer for the Giphy API with the following menu items
+// Search field allows to search gifs
+//     Include a pagination
+//         Previous and Next link
+//         Editable text field holding the current value
+//         Avoid passing non-existing pages
+//         Show number of pages
+//             Giphy API has an error loading with high offset
+// Show trending gifs
+//     Include a pagination
+// Make sure that the right amount of requests is sent
+// Show a loader text while the request is being sent
+// Use autorun and useLocalObservable for creating a MobX store automatically requesting required data
 
-const Container = styled.div``;
+// const API_KEY = "XdaoZq9JMeywVQvulWH47Yv2ZPAY6AC0";
 
-export const App = (): JSX.Element => {
-  const [json, setJson] = useState(null);
+const Container = styled.div`
+  display: 'flex';
+`;
 
-  const fetchURL = () => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=cat&limit=25&offset=0&rating=g&lang=en`;
-    console.log("fetch");
-    fetch(url).then((response) => response.json());
-  };
+function createStore(): Store {
+    return new Store();
+}
 
-  return (
-    <Container>
-        <GlobalStyles/>
-        <NavigationHeader/>
-    </Container>
-  );
-};
+export const App = observer(
+    (): JSX.Element => {
+        const store = useLocalObservable(createStore);
+
+        //TODO: implement useEffect hook to destroy store
+        useEffect(() => {
+          if(store.currentScreen === 'Trending') {
+            store.fetch();
+          }
+        }, [store, store.currentScreen]);
+
+        return (
+            <Container>
+                <GlobalStyles />
+                {console.log('in render: ' + store.gifs.map((item) => item.title + " " + item.url))}
+                <NavigationHeader />
+
+                    <>
+                        {store.gifs.map((item, index) => {
+                            {console.log('in map: ' + store.gifs.map((item) => item.title))}
+                            <>
+                            <p>Title:</p>
+                            <p key={index}>{item.title}</p>;
+                            </>
+                        })}
+                    </>
+                
+            </Container>
+        );
+    }
+);
--- assignment08/src/model/Store.tsx
+++ assignment08/src/model/Store.tsx
@@ -1,7 +1,68 @@
-import { action, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
+
+const API_KEY = 'XdaoZq9JMeywVQvulWH47Yv2ZPAY6AC0';
+const BASE_URL = 'https://api.giphy.com/v1/gifs/';
+const LIMIT = 13;
+
+export type Gifs = {
+    title: string;
+    url: string;
+};
 
 export class Store {
+    @observable gifs: Gifs[];
+    @observable currentScreen: string;
+    @observable searchString: string;
+    @observable isLoading: boolean;
+    @observable currentPage: number;
 
+    constructor() {
+        makeObservable(this);
+        this.gifs = [];
+        this.currentScreen = 'Trending';
+        this.searchString = '';
+        this.isLoading = false;
+        this.currentPage = 1;
+    }
 
+    @action fetch(): void {
+        let url: string;
+        if (this.currentScreen !== 'Trending') {
+            if (!this.searchString) {
+                return;
+            }
+            //URL to call API for SEARCH request
+            url = BASE_URL + `search?api_key=${API_KEY}&limit=${LIMIT}&q=${this.searchString}`;
+        } else {
+            //URL to call API for TRENDING request
+            url = BASE_URL + `trending?api_key=${API_KEY}&limit=${LIMIT}`;
+        }
 
+        this.isLoading = true;
+        console.log('fetching');
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                this.gifs = json.data.map((item) => {
+                    return {
+                        url: item.images.fixed_height.url,
+                        title: item.title,
+                    };
+                });
+                console.log('after fetch: ' + this.gifs.map((item) => item.title));
+                this.isLoading = false;
+            })
+            .catch((error) => console.log(error));
+    }
+
+    @action search(input: string): void {
+        this.reset();
+        this.searchString = input;
+        this.fetch();
+    }
+
+    //resets gif storage
+    reset(): void {
+        this.gifs = [];
+    }
 }
