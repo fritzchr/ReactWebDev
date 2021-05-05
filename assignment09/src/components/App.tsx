6b6351d06e8a65f154d5b196210ba91ef7aa1d8c
Christoph Fritz <fritz.chris@hotmail.com>
2021-05-05 10:33:22 +0200

    replaced App component setup

--- assignment09/src/components/App.tsx
+++ assignment09/src/components/App.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { observer, useLocalObservable } from 'mobx-react';
 import { Store } from '../model/Store';
 import { GlobalStyles } from '../styles/GlobalStyles';
-import { Container } from '../styles/Container';
 import { NavigationHeader } from './NavigationHeader';
-import { Content } from './Content';
-import { LoadingView } from './LoadingView';
-import { Pagination } from './Pagination';
-import { SearchField } from '../styles/SearchField';
-import { ContentContainer } from '../styles/ContentContainer';
 
 function createStore(): Store {
     return new Store();
@@ -18,58 +12,11 @@ export const App = observer(
     (): JSX.Element => {
         const store = useLocalObservable(createStore);
 
-        useEffect(() => {
-            if (store.currentScreen === 'Trending') {
-                store.fetch();
-            }
-        }, [store, store.currentScreen]);
-
-        const setScreen = (screen: string): void => {
-            store.changeScreen(screen);
-        };
-
-        const setPage = (page: number): void => {
-            store.setPage(page);
-            store.fetch();
-        }
-
-        const getCurrentPageNumber = (): string => {
-            return store.getCurrentPageNumber;
-        }
-
         return (
-            <Container>
-                <GlobalStyles />
-                <NavigationHeader setScreen={setScreen} />
-                <ContentContainer>
-                    {store.currentScreen === 'Trending' ? (
-                        <>
-                            {store.isLoading ? <LoadingView /> : <Content content={store.gifs}></Content>}
-                        </>
-                    ) : (
-                        <>
-                            <SearchField
-                                type='text'
-                                name='search'
-                                placeholder='Search...'
-                                onChange={(event) => store.search(event.target.value)}
-                                value={store.searchString}
-                            />
-                            {store.isLoading ? <LoadingView /> : <Content content={store.gifs} />}
-                        </>
-                    )}
-                </ContentContainer>
-                {store.currentScreen === 'Search' && store.searchString === '' ? 
-                    <div /> 
-                : 
-                    <Pagination 
-                        setPage={setPage}
-                        getCurrentPage={getCurrentPageNumber}
-                        maxPage={store.maxPage}
-                        currentPage={store.currentPage} 
-                    />
-                }
-            </Container>
+            <>
+            <GlobalStyles/>
+            <NavigationHeader/>
+            </>
         );
     }
 );
