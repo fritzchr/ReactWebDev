03ee062e7b47806e09fc4caf33b16a20c9803cb8
cfr2861 <cfr2861@students.fhv.at>
2021-05-05 21:01:18 +0200

    added currentDate for initialization

--- assignment09/src/components/DateView.tsx
+++ assignment09/src/components/DateView.tsx
@@ -4,6 +4,7 @@ import { ContentContainer } from '../styles/ContentContainer';
 import { Headline } from '../styles/Headline';
 import { InputField } from '../styles/InputField';
 
+//current date for initialization 
 const currentDate = new Date().toLocaleDateString('en');
 
 export const DateView = (): JSX.Element => {
@@ -12,7 +13,6 @@ export const DateView = (): JSX.Element => {
     const changeDate = (date: string) => {
         setDate(date);
     };
-
 
     return (
         <ContentContainer>
