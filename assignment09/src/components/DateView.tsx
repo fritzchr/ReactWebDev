4016225f52ec9e10ab27b42e955e36e704b94235
Christoph Fritz <fritz.chris@hotmail.com>
2021-05-06 13:17:20 +0200

    finished DateView

--- assignment09/src/components/DateView.tsx
+++ assignment09/src/components/DateView.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { FormattedDate } from 'react-intl';
 import { ContentContainer } from '../styles/ContentContainer';
 import { Headline } from '../styles/Headline';
 import { InputField } from '../styles/InputField';
+import { TextContainer } from '../styles/TextContainer';
 
-//current date for initialization 
+//current date for initialization
 const currentDate = new Date().toLocaleDateString('en');
 
 export const DateView = (): JSX.Element => {
@@ -16,9 +17,17 @@ export const DateView = (): JSX.Element => {
 
     return (
         <ContentContainer>
-            <InputField type='date' onChange={(event) => changeDate(event.target.value)}/>
+            <InputField type='date' onChange={(event) => changeDate(event.target.value)} />
             <Headline>Date: </Headline>
-            <FormattedDate value={new Date(date)}/>
+            <TextContainer>
+                <FormattedDate value={new Date(date)} />
+            </TextContainer>
+            <TextContainer>
+                <FormattedDate value={new Date(date)} year='numeric' month='long' day='2-digit' />
+            </TextContainer>
+            <TextContainer>
+                <FormattedDate value={new Date(date)} year="2-digit" month="short" day="numeric"/>
+            </TextContainer>
         </ContentContainer>
     );
 };
